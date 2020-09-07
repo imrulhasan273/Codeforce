88e778db0310771ed3010bd65d71b1627512c8ce
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-09-07 11:35:11 +0600

    A1006 + A630

--- A1006.py
+++ A1006.py
@@ -1,14 +1,17 @@
 __author__ = 'Md. Imrul Hasan @UAP-CSE'
 """A. Adjacent Replacements"""
 
-def func(a, b):
-    return 1
-    
+def func(n, arr):
 
+    for i in range(0, n):
+        if arr[i] % 2 == 0:
+            arr[i]-=1
+            
+    return " ".join(str(_) for _ in arr)
+    
 
 if __name__ == "__main__":
     
-    t = int(input())
-    for _ in range(0, t):
-        a, b = map(int, input().split(" "))
-        print(func(a, b))
+    n = int(input())    
+    arr = list(map(int, input().split(" ")))
+    print(func(n, arr))
---
+++ A630.py
@@ -0,0 +1,9 @@
+__author__ = 'Md. Imrul Hasan @UAP-CSE'
+"""A. Again Twenty Five!"""
+
+
+if __name__ == "__main__":
+    
+    n = int(input())    
+    print("25")
+
